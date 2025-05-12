2948aea57fdb65bfc11e23bf0f00f21407e51103
Aditya <jainaditya2309@gmail.com>
2025-05-12 15:44:16 +0530

    feat: update resume link in Button component and add new resume file

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = () => {
     return (
         <div className="btn flex gap-2 mt-3">
-            <div className=" bg-white  rounded-md pl-0.5 pr-0.5 text-sm pt-0.5 pb-0.5  justify-center"> <a href="/Aditya Jain Resume.docx"> <button className=" text-sm p-3 border-solid  rounded-md bg-violet-900"> Resume</button></a></div>
+            <div className=" bg-white  rounded-md pl-0.5 pr-0.5 text-sm pt-0.5 pb-0.5  justify-center"> <a href="/Aditya Resume.pdf"> <button className=" text-sm p-3 border-solid  rounded-md bg-violet-900"> Resume</button></a></div>
             <div className=" bg-white  rounded-md pl-0.5 pr-0.5 text-sm pt-0.5 mt-1 justify-center"> <a href="https://github.com/ADITYA9456"><button className=" text-sm p-2.5 border-solid  rounded-md bg-violet-900">Visit Github</button></a></div>
         </div>
     )
